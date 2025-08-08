ddd208946c4dabf1af33238475896818ec02a849
gingerale911 <sr.00017000@gmail.com>
2025-08-08 19:11:51 +0530

    Verification bypass

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()  # load environment variables from .env file
 print('GOOGLE_API_KEY:', os.getenv('GOOGLE_API_KEY'))
 from fastapi import FastAPI
@@ -16,14 +15,14 @@ model = genai.GenerativeModel("models/gemini-1.5-flash-8b")
 app = FastAPI()
 
 class QuestionRequest(BaseModel):
-    question: str
+question: str
 
 @app.get("/")
 async def hello():
-    return {"hello": "world"}
+return {"hello": "world"}
 
 @app.post("/")
 async def answer_with_llm(request: QuestionRequest):
-    text = request.question
-    resp = model.generate_content(text)
-    return JSONResponse([resp.text])
+text = request.question
+resp = model.generate_content(text)
+return JSONResponse([resp.text])
